8b07bb6ab748f444bfac3677bb67d58e1822cd15
drikin <drikin@gmail.com>
2015-03-26 23:39:22 -0700

    add mac icns

--- build.js
+++ build.js
@@ -2,7 +2,9 @@ var NwBuilder = require('node-webkit-builder');
 var nw = new NwBuilder({
     buildDir: 'dist',
     files: ['package.json', 'index.html', 'stage.css', 'stage.js', 'node_modules/jquery/**'],
-    platforms: ['win64', 'osx64']
+    platforms: ['win64', 'osx64'],
+    macIcns: 'fbm.icns',
+    macZip: true
     //winIco: 'res/slack.ico'
 });
 
