9b7a42c327615944e6dbbf25d9de26aa12ec81c9
Koh Aoki <Kohichi.Aoki@am.sony.com>
2015-04-09 16:26:57 -0700

    enable Notification on Windows env
    also cleaning code a bit

--- stage.js
+++ stage.js
@@ -1,10 +1,10 @@
 (function() {
   var $ = require('jquery');
   var gui = require('nw.gui');
+  gui.App.createShortcut(process.env.APPDATA + "\\Microsoft\\Windows\\Start Menu\\Programs\\node-webkit.lnk");
 
   // Get the current window
   var win = gui.Window.get();
-
   win.on('new-win-policy', function(frame, url, policy) {
     policy.ignore();
     gui.Shell.openExternal(url);
@@ -18,47 +18,7 @@
 
     i.onload = function() {
         console.log('loaded');
-        $(d.head).contents().append("<style>body {font-family: helvetica, arial,'lucida grande', 'Hiragino Kaku Gothic ProN', '游ゴシック', YuGothic, Meiryo, sans-serif !important;} .panelFlyout {right: 0 !important;}</style>");
-
-        var topbar = d.getElementById("pagelet_bluebar");
-        if (topbar && topbar.style) {
-          topbar.style.display = "none";
-        }
-        var sidebar = d.getElementById("pagelet_sidebar");
-        if (sidebar && sidebar.style) {
-          sidebar.style.display = "none";
-        }
-        var dock = d.getElementById("pagelet_dock");
-        if (dock && dock.style) {
-          dock.style.display = "none";
-        }
-        var ad = d.getElementById("rightCol");
-        if (ad && ad.style) {
-          ad.style.display = "none";
-        }
-
-        // observe message notification count on the blue bar
-        var permission = Notification.permission;
-        var messageBadgeCountTag = d.querySelector('#mercurymessagesCountValue');
-        if (messageBadgeCountTag) {
-          var badgeCountObserver = new MutationObserver(function(mutations) {
-            mutations.forEach(function(mutation) {
-              var addedNodes = mutation.addedNodes;
-              if (addedNodes && addedNodes.length ===1) {
-                var count = parseInt(addedNodes[0].data);
-                if (count) {
-                  win.setBadgeLabel(count);
-                  if (permission === "granted") {
-                    var n = new Notification(d.title);
-                  }
-                } else {
-                  win.setBadgeLabel("");
-                }
-              }
-            });
-          });
-          badgeCountObserver.observe(messageBadgeCountTag, { childList: true });
-        }
+        $(d.head).contents().append("<style>body {font-family:'Noto Sans CJK JP', helvetica, arial,'lucida grande', 'Hiragino Kaku Gothic ProN', '游ゴシック', YuGothic, Meiryo, sans-serif !important;}</style>");
 
         // ready to show
         var root = document.getElementById("main");
